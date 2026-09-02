425a9b6cdc077b05a21a1f9e942e47a0cdd07d52
vincentnordlund-commits <vincent.nordlund@skola.solvesborg.se>
2026-09-02 09:47:22 +0200

    Sum bs

--- control.gd
+++ control.gd
@@ -1,9 +1,12 @@
 extends Control
 
+
+
 func _on_button_pressed() -> void:
-	print("Yeetus")
-	get_tree().change_scene_to_file("res://De Fyra Räknesätten.tscn")
+	get_tree().change_scene_to_file("res://de_fyra_räknesätten.tscn")
+	
 	
 
+
 func _on_button_2_pressed() -> void:
-	get_tree().change_scene_to_file("res://thingymabob.tscn")
+	get_tree().change_scene_to_file("res://yes.tscn")
