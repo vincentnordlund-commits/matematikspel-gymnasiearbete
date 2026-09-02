348f80c9caa1d7cbdf182d34a52ed521cc2bc0d9
Autistic-monkey67 <emil.nordahl@skola.solvesborg.se>
2026-09-02 09:26:25 +0200

    rigger

--- control.gd
+++ control.gd
@@ -4,5 +4,9 @@ extends Control
 
 
 func _on_button_pressed() -> void:
-	print("Yeetus")
 	get_tree().change_scene_to_file("res://node_2d.tscn")
+
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://thingymabob.tscn")
