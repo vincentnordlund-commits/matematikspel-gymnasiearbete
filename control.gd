a0828e6aa4ead90ff3d19413cd998eaf8998855c
vincentnordlund-commits <vincent.nordlund@skola.solvesborg.se>
2026-09-02 09:28:34 +0200

    button

--- control.gd
+++ control.gd
@@ -1,8 +1,10 @@
 extends Control
 
-
-
-
 func _on_button_pressed() -> void:
 	print("Yeetus")
-	get_tree().change_scene_to_file("res://node_2d.tscn")
+	get_tree().change_scene_to_file("res://De Fyra Räknesätten.tscn")
+	
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://thing.tscn")
