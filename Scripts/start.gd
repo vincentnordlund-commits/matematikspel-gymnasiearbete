e34c66e11468602f217968377b9b1c9f88b4a75f
vincentnordlund-commits <vincent.nordlund@skola.solvesborg.se>
2026-09-03 12:42:24 +0200

    Pushx2

--- Scripts/start.gd
+++ Scripts/start.gd
@@ -5,5 +5,7 @@ extends Control
 func _on_spel_1_pressed() -> void:
 	if spel_3.visible == false:
 		$"Spel 1/Spel 3".show()
+		$"Spel 1/Spel 4".show()
 	else:
 		$"Spel 1/Spel 3".hide()
+		$"Spel 1/Spel 4".hide()
