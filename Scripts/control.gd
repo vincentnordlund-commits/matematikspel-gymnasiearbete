8b406c37288aa4cda036d36de07edd1b7fa48cb4
vincentnordlund-commits <vincent.nordlund@skola.solvesborg.se>
2026-09-03 08:46:20 +0200

    Jebus

---
+++ Scripts/control.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/de_fyra_räknesätten.tscn")
+	
+	
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/yes.tscn")
