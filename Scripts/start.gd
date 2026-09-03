9758d8e5cc0d7b31eaa9feed884902dbe2e35677
vincentnordlund-commits <vincent.nordlund@skola.solvesborg.se>
2026-09-03 12:40:57 +0200

    Piush

--- Scripts/start.gd
+++ Scripts/start.gd
@@ -3,4 +3,7 @@ extends Control
 @onready var spel_3: TextureButton = $"Spel 1/Spel 3"
 
 func _on_spel_1_pressed() -> void:
-	
+	if spel_3.visible == false:
+		$"Spel 1/Spel 3".show()
+	else:
+		$"Spel 1/Spel 3".hide()
